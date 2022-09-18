8bbd7fd2fe266f63fb6e765e80d85282a71f077d
Ahmed12ibad <99754515+Ahmed12ibad@users.noreply.github.com>
2022-09-18 13:25:09 -0700

    1

---
+++ app.js
@@ -0,0 +1,85 @@
+
+var arr =[]
+function submit() {
+   
+    var name = document.getElementById("name")
+    var fatherName = document.getElementById("father-name")
+    var email = document.getElementById("email")
+    var cnic = document.getElementById("cnic")
+    var phone_number = document.getElementById("phone-number")
+    var male = document.getElementById("male")
+    var female = document.getElementById("female")
+    var country = document.getElementById("country")
+    // var pro = document.getElementById("programing")
+
+    
+
+    var nameRegix = /^[a-zA-Z ]+$/.test(name.value);
+    var fatherRegix = /^[a-zA-Z ]+$/.test(fatherName.value);
+    var emailRegix = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email.value);
+    // var cnicRegix = /^[0-9]{5}-[0-9]{7}-[0-9]$/.test(cnic.value);
+   var phoneregix =/^((\+92)?(0092)?(92)?(0)?)(3)([0-9]{9})$/gm.test(phone_number.value)
+
+  
+
+
+
+
+   if (nameRegix) {
+
+    if(fatherRegix){
+        
+    if (emailRegix) {
+
+    if (cnic.value) {
+
+    if(phoneregix){
+
+    if(male.checked || female.checked) {
+    
+     if(country){
+
+
+      
+     }else{alert("country is required")}   
+    }else{alert("gender is required")}   
+    }else{alert("emter velid number")}
+    }else{ alert("Enter valid cnic")}
+    }else{alert("Enter valid email")} 
+    }else{alert("enter velid father name")}
+    }else{alert("Enter Valid name")}
+
+    var object =
+        {
+            name: name.value,
+            father: fatherName.value,
+            email: email.value,
+            cnic: cnic.value,
+            phone:phone_number.value,
+            city_name : country.value,
+            
+// ye do baki he
+
+            // lengeuges : pro.value,
+            // gender : male.valus
+
+        }
+        
+    
+    arr.push(object)
+        // window.setTimeout(function(){
+            localStorage.setItem("information",JSON.stringify(arr))
+            // window.location ="print.html"
+        // },2000)
+            // console.log("uroobamisbah");
+ 
+     
+    
+ 
+
+    //
+
+
+    
+}
+
---
+++ print.js
@@ -0,0 +1,105 @@
+
+    var info = localStorage.getItem("information")
+    if(info){
+    var arr1 = JSON.parse(info)
+console.log(arr1);
+    var concat = document.getElementById('con-cat')
+
+    for(var i=0; i<arr1.length; i++){
+        console.log(arr1[i]);
+var ahmed =
+ `<tr class="tr">
+ <td>${arr1[i].name}
+</td>
+<td>${arr1[i].father}
+ </td>
+<td>${arr1[i].email}
+ </td>
+<td>${arr1[i].cnic}
+ </td>
+<td>${arr1[i].phone} 
+</td>
+<td>${arr1[i].city_name}
+ </td>
+<td> <button onclick="delet('${i}',this)">delete</button>
+ </td>
+
+</tr>`;
+concat.innerHTML+=ahmed
+
+
+    }
+    
+    }
+
+    var delet = (i,thi) => {
+        thi.parentNode.parentNode.remove()
+// console.log(thi.parentNode.parentNode.remove()
+arr1.splice(i,1)
+localStorage.setItem("information",JSON.stringify(arr1))
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function foo (){
+
+//     var info = localStorage.getItem("information")
+//     if(info){
+//     var obj = JSON.parse(info)
+//     var object = Object.values(obj)
+//     // console.log(object)
+//     var name = document.getElementById("print-name")
+//     var father = document.getElementById("print-father-name")
+//     var email = document.getElementById("print-email")
+//     var cnic = document.getElementById("print-cnic")
+//     var number = document.getElementById("print-number")
+//     var city = document.getElementById("print-city")
+//     //
+//     name.innerHTML = object[0]
+//     name.style.textAlign="center"
+//     //
+//     father.innerHTML = object[1]
+//     father.style.textAlign="center"
+//     //
+//     email.innerHTML = object[2]
+//     email.style.textAlign="center"
+//     //
+//     cnic.innerHTML = object[3]
+//     cnic.style.textAlign="center"
+//     //
+//     number.innerHTML = object[4]
+//     number.style.textAlign="center"
+//     //
+//     city.innerHTML = object[5]
+//     city.style.textAlign="center"
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//     }
+    
+//     }
+    
